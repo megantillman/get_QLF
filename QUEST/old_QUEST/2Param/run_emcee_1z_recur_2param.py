4169bb5af204757b8bb9fa2af15b428b689f6a1b
Megan Taylor Tillman <megantillman@Megans-MacBook-Pro-3.local>
2020-08-19 17:24:34 -0500

    Get rid of old files/rename some things.

--- QUEST/old_QUEST/2Param/run_emcee_1z_recur_2param.py
+++
@@ -1,9 +0,0 @@
-from run_emcee_1z_2param import *
-import sys
-
-redshift = float( sys.argv[1] )
-print( 'Running {}'.format( redshift ) )
-filename = "output/chain-data_2Param_z="+str(redshift)+".h5py"
-    
-    
-run_emcee(redshift, filename)
